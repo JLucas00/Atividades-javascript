926ebce60efa12a91aa6a7d956ed09d83b3cb3b6
João Lucas <jlucastbe100@gmail.com>
2021-11-19 05:31:51 -0300

    atividade hobby

---
+++ atividade-6/script.js
@@ -0,0 +1,22 @@
+document.getElementById("titulo").innerHTML = 'Meu maior Hobby';
+
+
+document.getElementById("perfil").setAttribute("src" , "./assets/images/foto1.jpg");
+document.getElementById("nome").innerHTML = 'João Lucas de Alcântara';
+document.getElementById("idade").innerHTML = '25 anos';
+document.getElementById("estado").innerHTML = 'Santa Cruz do Capibaribe - PE';
+
+document.getElementById("hobby").innerHTML = 'RPG de mesa';
+document.getElementById("descrição").innerHTML = 'O RPG de mesa, ou RPG de papel e caneta, é um jogo que lembra muito os clássicos jogos de tabuleiro, porém com possibilidades mais amplas. Do inglês, a sigla RPG significa "Role Playing Game", um jogo de interpretar papéis. Neste caso o jogador não é apenas um pino andando em casas de um tabuleiro, ele interpreta um personagem, com emoções, falas e habilidades enquanto atravessa um mundo fantástico. Para mais informações e curiosidades acesse o link ao lado! <a id="link1"></a>';
+
+document.getElementById("link1").setAttribute("href" , "https://www.zoom.com.br/jogos/deumzoom/rpg-de-mesa");
+document.getElementById("link1").innerHTML = 'Clique aqui';
+
+
+document.getElementById("imagem1").setAttribute("src" , "https://caixinhaquantica.com.br/wp-content/uploads/2019/07/Capa-Arte-RPG-scaled.jpg");
+document.getElementById("imagem2").setAttribute("src" , "https://miro.medium.com/max/862/1*1mDPyZyZj5TU4s_exczIyw.jpeg");
+
+
+
+
+
