8e530392b77026cae5a19df13cadae708463d0e4
João Lucas <jlucastbe100@gmail.com>
2021-11-22 10:06:37 -0300

    pagina banda favorita

---
+++ atividade-7/script.js
@@ -0,0 +1,86 @@
+
+
+/* Estilização do body */
+document.querySelector("body").style.backgroundColor = "#111111";
+document.querySelector("body").style.color = "#ffffff";
+document.querySelector("body").style.width = "100%";
+document.querySelector("body").style.height = "200%";
+document.querySelector("body").style.fontFamily = "proxima-nova";
+document.querySelector("body").style.display = "flex";
+document.querySelector("body").style.flexDirection = "column";
+
+
+
+/* Estilização do header */
+document.querySelector("header").style.display= "flex";
+document.querySelector("header").style.width= "100%";
+document.querySelector("header").style.height= "25vh";
+document.querySelector("header").style.justifyContent= "center";
+document.querySelector("header").style.alignItems= "center";
+
+/* Estilização da imagem do header */
+document.querySelector(".imagem-logo-metallica").style.height= "80%";
+
+
+/* Estilização da seção do meu perfil */
+document.querySelector(".meu-perfil").style.width= "100%";
+document.querySelector(".meu-perfil").style.height= "max-content";
+document.querySelector(".meu-perfil").style.display= "flex";
+document.querySelector(".meu-perfil").style.flexDirection= "column";
+document.querySelector(".meu-perfil").style.justifyContent= "center";
+document.querySelector(".meu-perfil").style.alignItems= "center";
+document.querySelector(".meu-perfil").style.marginBottom= "50px";
+
+/* Estilização dos textos da seção do meu perfil */
+let elementosTextuaisPerfil = document.querySelectorAll(".texto-meu-perfil");
+elementosTextuaisPerfil[0].style.margin= "10px";
+elementosTextuaisPerfil[0].style.textAlign= "justify";
+elementosTextuaisPerfil[1].style.margin= "10px";
+elementosTextuaisPerfil[1].style.textAlign= "justify";
+elementosTextuaisPerfil[2].style.margin= "10px";
+elementosTextuaisPerfil[2].style.textAlign= "justify";
+
+/* Estilização da foto de perfil */
+document.querySelector(".imagem-perfil").style.width= "15%";
+document.querySelector(".imagem-perfil").style.minWidth= "150px";
+document.querySelector(".imagem-perfil").style.border= "5px ridge white";
+document.querySelector(".imagem-perfil").style.margin= "20px";
+
+/* Estilização da seção do Metallica */
+document.querySelector(".perfil-metallica").style.width= "100%";
+document.querySelector(".perfil-metallica").style.minWidth= "200px";
+document.querySelector(".perfil-metallica").style.height= "max-content";
+document.querySelector(".perfil-metallica").style.display= "flex";
+document.querySelector(".perfil-metallica").style.padding= "10px";
+document.querySelector(".perfil-metallica").style.flexDirection= "column";
+document.querySelector(".perfil-metallica").style.justifyContent= "center";
+document.querySelector(".perfil-metallica").style.alignItems= "center";
+document.querySelector(".perfil-metallica").style.textAlign= "justify";
+
+
+/* Estilização iamgem do Metallica */
+document.querySelector(".imagem-metallica").style.width= "40%";
+document.querySelector(".imagem-metallica").style.minWidth= "300px";
+document.querySelector(".imagem-metallica").style.border= "5px ridge white";
+document.querySelector(".imagem-metallica").style.margin= "20px";
+
+/* Estilização dos textos da seção do Metallica */
+document.querySelector(".titulo1").style.fontSize= "1.5em";
+document.querySelector(".metallica").style.fontSize= "1.8em";
+document.querySelector(".metallica").style.marginBottom= "30px";
+document.querySelector(".texto-metallica").style.width= "40%";
+document.querySelector(".texto-metallica").style.minWidth= "300px";
+
+
+/* Estilização seção músicas favoritas */
+document.querySelector(".musicas-favoritas").style.width= "100%";
+document.querySelector("ul").style.width= "40%";
+document.querySelector("ul").style.minWidth= "300px";
+document.querySelector(".musicas-favoritas").style.minWidth= "200px";
+document.querySelector(".musicas-favoritas").style.height= "max-content";
+document.querySelector(".musicas-favoritas").style.display= "flex";
+document.querySelector(".musicas-favoritas").style.padding= "10px";
+document.querySelector(".musicas-favoritas").style.flexDirection= "column";
+document.querySelector(".musicas-favoritas").style.justifyContent= "center";
+document.querySelector(".musicas-favoritas").style.alignItems= "center";
+document.querySelector(".musicas-favoritas").style.textAlign= "justify";
