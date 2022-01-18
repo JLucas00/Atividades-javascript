47f4d7818bbfa7e14c5014ce093a9ccbcb9477e8
João Lucas <jlucastbe100@gmail.com>
2022-01-18 08:24:13 -0300

    atividade rest operator

---
+++ atividade-25/script.js
@@ -0,0 +1,10 @@
+function sortArray(...args){
+   
+   const ordenedArray = args.sort();
+
+   console.log(`Os parametros ordenados no array são:`);
+   console.log(ordenedArray)
+}
+sortArray(7,2,5, "Leonardo", "Bruno", "Vitória", "Arthur" );
+
+
