4bacc0516344f6fd04fee08732cc4912606e77a1
João Lucas <jlucastbe100@gmail.com>
2022-02-11 12:11:54 -0300

    raparo no servidor do frontend

--- atividade-30/frontend/server.js
+++ atividade-30/frontend/server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const app = express();
-const app2 = express();
-const app3 = express();
-const port1 = 8080;
-const port2 = 8081;
-const port3 = 8083;
+
+const port = 8080;
 
 
-app.use(express.static('./src/funcionarios'));
-app.listen(port1, () => console.log(`Example app listening on port ${port1}!`));
 
-app2.use(express.static('./src/calculadora'));
-app2.listen(port2, () => console.log(`Example app listening on port ${port2}!`));
+app.use('/funcionarios', express.static('./src/funcionarios'));
+app.use('/calculadora', express.static('./src/calculadora'));
+app.use('/gamesmagazine',express.static('./src/gamesmagazine'));
 
-app3.use(express.static('./src/gamesmagazine'));
-app3.listen(port3, () => console.log(`Example app listening on port ${port3}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
